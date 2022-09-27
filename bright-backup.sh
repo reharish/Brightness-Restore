cd68a15cf70bfb963ff903e43e52de976dc4cdf1
reharish <58879939+reharish@users.noreply.github.com>
2022-09-27 19:52:44 +0530

    Minor Fixes

--- bright-backup.sh
+++ bright-backup.sh
@@ -6,7 +6,7 @@ c_bright=`cat /sys/class/backlight/$BRIGHT_RES_DRIVER/brightness`
 
 if [ "$c_bright" != "" ]
 then
-    echo $c_bright | tee /opt/Bright-Res/brightness.bak
+    echo $c_bright | tee /opt/Bright-Res/brightness.bak > /dev/null
 else
     echo "Couldn't take Brightness backup"
 fi
